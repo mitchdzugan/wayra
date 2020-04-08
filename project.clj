4d84f17eaebe2b1e4a82548f613df266e87cac89
Mitch Dzugan <mitchdzugan@gmail.com>
2020-04-07 23:21:02 -0500

    Version 0.0.10

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wayra "0.0.9"
+(defproject wayra "0.0.10"
   :description "FIXME: write description"
   :url "https://github.com/mitchdzugan/wayra"
   :license {:name "Eclipse Public License"
