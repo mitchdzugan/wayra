954b00489da95e95de1d02af1bc6bb40a10b0958
Mitch Dzugan <mitchdzugan@gmail.com>
2020-04-08 05:52:12 -0500

    lein ancient upgrade :check-clojure

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
             [lein-shell "0.5.0"]
             [lein-ancient "0.6.15"]
             [lein-changelog "0.3.2"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}}
   :deploy-repositories [["releases" :clojars]]
   :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[wayra \"[0-9.]*\"\\\\]/[wayra \"${:version}\"]/" "README.md"]}
   :release-tasks [["shell" "git" "diff" "--exit-code"]
